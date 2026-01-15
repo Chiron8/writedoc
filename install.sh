e905ae421b0c06c24aeedeb230f99613a41d8897
Chiron Horwood <chiron.dev8@gmail.com>
2026-01-15 13:31:24 +0000

    fix bugs + security issues in install.sh, #... only in .md files

--- install.sh
+++ install.sh
@@ -8,7 +8,19 @@ install -Dm755 writedoc /usr/local/bin/writedoc
 echo "Installing man page to /usr/local/share/man/man1..."
 install -Dm644 writedoc.1 /usr/local/share/man/man1/writedoc.1
 
-echo "Updating man database..."
-# mandb || true  # Uncomment this if man page can't be found
+#echo "Updating man database..."
+#mandb || true  # Uncomment this if man page can't be found
+
+echo "Installing configs"
+mkdir /usr/local/share/writedoc
+
+cp directory.txt /usr/share/writedoc/directory.txt
+chmod 644 /usr/share/writedoc/directory.txt
+
+cp editor.txt /usr/share/writedoc/editor.txt
+chmod 644 /usr/share/writedoc/editor.txt
+
+cp ft.txt /usr/share/writedoc/ft.txt
+chmod 644 /usr/share/writedoc/ft.txt
 
 echo "Finished installing! "
